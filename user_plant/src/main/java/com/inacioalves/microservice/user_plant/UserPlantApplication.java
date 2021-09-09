e832bcdaecffa02ced8dfd553c164519d86e4d3b
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-09-09 09:52:10 -0300

    first post user

---
+++ user_plant/src/main/java/com/inacioalves/microservice/user_plant/UserPlantApplication.java
@@ -0,0 +1,13 @@
+package com.inacioalves.microservice.user_plant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPlantApplication.class, args);
+	}
+
+}
---
+++ user_plant/src/test/java/com/inacioalves/microservice/user_plant/UserPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inacioalves.microservice.user_plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
