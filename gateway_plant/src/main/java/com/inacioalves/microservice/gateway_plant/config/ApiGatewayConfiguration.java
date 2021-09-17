1a2791d80e4b25265651c9273950b3eb859df405
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-09-17 16:50:15 -0300

    ApiGateway Get

---
+++ gateway_plant/src/main/java/com/inacioalves/microservice/gateway_plant/config/ApiGatewayConfiguration.java
@@ -0,0 +1,26 @@
+package com.inacioalves.microservice.gateway_plant.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f
+								.addRequestHeader("MyHeader", "MyURI")
+								.addRequestParameter("Param", "MyValue"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/plant/**")
+						.uri("lb://plant"))
+				.route(p -> p.path("/plant-conversion/**")//URL
+						.uri("lb://notifications"))//name application.properties
+				.build();
+	}
+}
