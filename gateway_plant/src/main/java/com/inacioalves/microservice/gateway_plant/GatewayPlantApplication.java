ad39823221c88722791b22d8f7bf2ae5132ad829
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-09-17 15:55:27 -0300

    gateway_plant

---
+++ gateway_plant/src/main/java/com/inacioalves/microservice/gateway_plant/GatewayPlantApplication.java
@@ -0,0 +1,13 @@
+package com.inacioalves.microservice.gateway_plant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPlantApplication.class, args);
+	}
+
+}
---
+++ gateway_plant/src/test/java/com/inacioalves/microservice/gateway_plant/GatewayPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inacioalves.microservice.gateway_plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
