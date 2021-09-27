35e3fee9b22ea79aa6d56a1ff7e6e637b1f0cc8f
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-09-27 10:58:08 -0300

    objectNotFound

---
+++ notify_plant/src/main/java/com/inacioalves/microservice/notify_plant/controller/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.inacioalves.microservice.notify_plant.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.inacioalves.microservice.notify_plant.exeption.objectNotFoundException;
+import com.inacioalves.microservice.notify_plant.model.StandarError;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(objectNotFoundException.class)
+	public ResponseEntity<StandarError>objectNotFound(objectNotFoundException e, HttpServletRequest request){
+		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(),e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
--- notify_plant/src/main/java/com/inacioalves/microservice/notify_plant/exeption/objectNotFoundException.java
+++ notify_plant/src/main/java/com/inacioalves/microservice/notify_plant/exeption/objectNotFoundException.java
@@ -1,10 +1,7 @@
 package com.inacioalves.microservice.notify_plant.exeption;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class objectNotFoundException extends Exception  {
+public class objectNotFoundException extends RuntimeException   {
 
 	
 	private static final long serialVersionUID = 1L;
